7a57eab8d3f6aee0d323bb84f7f4fd6fa59d32de
Michelle Poterek <poterekm@gmail.com>
2017-08-18 09:43:21 -0700

    add input to allow data to come down to child component

--- src/app/animal-list/animal-list.component.ts
+++ src/app/animal-list/animal-list.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Animal } from './../animal.model';
+
 
 @Component({
-  selector: 'app-animal-list',
+  selector: 'animal-list',
   templateUrl: './animal-list.component.html',
   styleUrls: ['./animal-list.component.css']
 })
 export class AnimalListComponent implements OnInit {
+  @Input() childAnimalList: Animal[];
+
 
   constructor() { }
 
