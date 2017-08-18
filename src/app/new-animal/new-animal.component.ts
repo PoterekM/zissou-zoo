fe270ad5dc948ac7260fca3809b4e274f1f7af20
Michelle Poterek <poterekm@gmail.com>
2017-08-18 11:39:01 -0700

    add a new animal object in submit form method

--- src/app/new-animal/new-animal.component.ts
+++ src/app/new-animal/new-animal.component.ts
@@ -7,10 +7,23 @@ import { Animal } from './../animal.model';
   styleUrls: ['./new-animal.component.css']
 })
 export class NewAnimalComponent implements OnInit {
+  @Output() newAnimalSender = new EventEmitter();
+
+  addingAnimal = false;
+
+  submitForm(species: string, name: string, age: number, diet: string, location: string, caretakers: number, sex: string, likes: string, dislikes: string) {
+    var newAnimalToAdd: Animal = new Animal(species, name, age, diet, location, caretakers, sex, likes, dislikes);
+    this.addingAnimal = false;
+    this.newAnimalSender.emit(newAnimalToAdd);
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addNewAnimal() {
+    this.addingAnimal = true;
   }
 
 }
