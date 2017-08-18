42a542e2268a9b6d4e273da89dd907f780b40e30
Michelle Poterek <poterekm@gmail.com>
2017-08-18 09:37:36 -0700

    generate new component to show animal list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Animal } from './animal.model'
 
 @Component({
@@ -7,5 +7,9 @@ import { Animal } from './animal.model'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Zooberz';
+  masterAnimalList: Animal [] = [
+    new Animal('species', 'name', 2, 'diet', 'location', 2, 'sex', 'likes', 'dislikes'),
+    new Animal('Tiger', 'Tony', 4, 'carnivore', 'SE Wing', 4, 'Female', 'lounging in sun', 'children climbing in for cuddles')
+  ]
 }
