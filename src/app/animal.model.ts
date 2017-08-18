40547de546a6fbfaeb72a318a879beef2d049431
Michelle Poterek <poterekm@gmail.com>
2017-08-18 09:08:34 -0700

    refactor model to make constructor parameters public

--- src/app/animal.model.ts
+++ src/app/animal.model.ts
@@ -1,11 +1,5 @@
-class Animal {
-  species: string;
-  name: string;
-  age: number;
-  diet: string;
-  location: string;
-  caretakers: number;
-  sex: string;
-  likes: string;
-  dislikes: string
-}
+export class Animal {
+
+constructor(public species: string, public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string) { };
+
+};
